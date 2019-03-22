924b4ac22f4637324722bf64604c81705958d674
shynz0 <gustavoddantas01@gmail.com>
2019-03-22 10:56:22 -0300

    teste de cliente/servidor

---
+++ serve/src/teste/Decrypt.java
@@ -0,0 +1,18 @@
+package teste;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+
+public class Decrypt{
+
+	String IV = "AAAAAAAAAAAAAAAA";
+	
+	public String decrypt(byte[] menD, String keys) throws Exception{
+		Cipher decrypt = Cipher.getInstance("AES/CBC/PKCS5Padding", "SunJCE");
+		SecretKeySpec key = new SecretKeySpec(keys.getBytes("UTF-8"), "AES");
+		decrypt.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF-8")));
+		return new String(decrypt.doFinal(menD),"UTF-8");
+	}
+	
+}
