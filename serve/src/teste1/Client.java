e15f50ade703e548a405ab195c3f1bb726c240da
João Gustavo <gustavoddantas01@gmail.com>
2019-03-28 20:57:45 -0300

    Arrumando poucas coisas ocomo identação e EXECUÇÃO DE PROGRAMAS FUNCIONANDO!!111

--- serve/src/teste1/Client.java
+++ serve/src/teste1/Client.java
@@ -13,42 +13,40 @@ import java.util.Scanner;
 import teste1.Encrypt;
 public class Client {
 	
-	public static void main(String[] args) 
-									throws UnknownHostException, IOException, Exception{
-		DataOutputStream out;
-		SecureRandom random = new SecureRandom();
-		Encrypt encrypt = new Encrypt();
-	    byte[] aesKey = new byte[16];
-	    random.nextBytes(aesKey);
-	    byte[] iv = new byte[16];
-	    random.nextBytes(iv);
-	    String aesKeyAsString = Base64.getEncoder().encodeToString(aesKey);
-	    String ivAsString = Base64.getEncoder().encodeToString(iv);
-		
-		Scanner t = new Scanner(System.in);
-		try {
-		Socket client = new Socket("127.0.0.1", 12345);
-		out = new DataOutputStream(client.getOutputStream());
-		String str;
+public static void main(String[] args) 
+					throws UnknownHostException, IOException, Exception{
+	DataOutputStream out;
+	SecureRandom random = new SecureRandom();
+	Encrypt encrypt = new Encrypt();
+	byte[] iv = new byte[16];
+	byte[] aesKey = new byte[16];
+	Scanner t = new Scanner(System.in);
+	try {
+	Socket client = new Socket("127.0.0.1", 12345);
+	out = new DataOutputStream(client.getOutputStream());
+	String str;
 		try {
 			while(true){
-				if(client.isConnected()) {
-					System.out.print(">");
-					str = t.nextLine();
-					byte[] mens = encrypt.encrypt(str, aesKeyAsString, ivAsString);
-					String encryptedString = Base64.getEncoder().encodeToString(mens);
-					out.writeUTF(aesKeyAsString);
-					out.flush();
-					out.writeUTF(ivAsString);
-					out.flush();
-					out.writeUTF(encryptedString);
-					out.flush();
-					str = " ";
-				} else {
-					break;
-					}
+			if(client.isConnected()) {
+			    random.nextBytes(aesKey);
+			    random.nextBytes(iv);
+			    String aesKeyAsString = Base64.getEncoder().encodeToString(aesKey);
+			    String ivAsString = Base64.getEncoder().encodeToString(iv);
+				System.out.print(">");
+				str = t.nextLine();
+				byte[] mens = encrypt.encrypt(str, aesKeyAsString, ivAsString);
+				String encryptedString = Base64.getEncoder().encodeToString(mens);
+				out.writeUTF(aesKeyAsString);
+				out.flush();
+				out.writeUTF(ivAsString);
+				out.flush();
+				out.writeUTF(encryptedString);
+				out.flush();
+				str = " ";
+			} else {
+				break;
 				}
-			
+			}
 		} catch (SocketException e) {
 			System.out.println("Conexão encerrada");
 			client.close();
@@ -57,8 +55,6 @@ public class Client {
 		System.out.println("Erro! Socket não encontrado.");
 		
 	}
-		t.close();
-		
+		t.close();	
 	}
-
 }
--- serve/src/teste1/Server.java
+++ serve/src/teste1/Server.java
@@ -9,52 +9,69 @@ import java.net.SocketException;
 
 public class Server {
 
-	public static void main(String[] args) throws IOException, Exception {
-			DataInputStream in;
-			ServerSocket servidor = new ServerSocket(12345);
-			System.out.println("Porta 12345 aberta!");
-			String mens, aesB, ivB, mend;
-			boolean arq = false;
-			Decrypt decrypt = new Decrypt();
-			Socket cliente = servidor.accept();
-			System.out.println("Nova conexão com o cliente " +     
-			cliente.getInetAddress().getHostAddress());
-			in = new DataInputStream(cliente.getInputStream());
-			try {
-				while(!servidor.isClosed()) {
-				aesB = in.readUTF();
-				ivB = in.readUTF();
-				mens = in.readUTF();
-				mend = decrypt.decrypt(mens, aesB, ivB);
-					if(mend.equals("te amo")) {
-						System.out.println("eu sei que você me ama.");
-					} else if(mend.equals("fechar")) {
-						System.out.println("Conexão encerrada.");
-						servidor.close();
-					} else if(mend.contains("abrir")) {
-						arq = true;
-						while(arq) {
-							System.out.println("O que deseja abrir?");
-							listarquivos();
-	
-						}
-					} else {
-						System.out.println("Mensagem: " + mend);
-					}
-				}
-			} catch (SocketException e) {
-				System.out.println("Conexão perdida.");
-				servidor.close();
-			}
-         }
-	public static void listarquivos() {
-		File folder = new File("C:\\Users\\Usuário\\Desktop\\programas");
-		 
-        String[] files = folder.list();
- 
-        for (String file : files)
-        {
-            System.out.println("\t>" + file);
-        }
+public static void listarquivos() {
+  File folder = new File("C:\\Users\\Usuário\\Desktop\\programas");
+  String[] files = folder.list();
+  for (String file : files){
+    System.out.println("\t>" + file);
+  }
+}
+
+public static void exec(String a) {
+	try {
+		String[] command = {"cmd.exe", "/C", "Start", "C:\\Users\\Usuário\\Desktop\\programas\\".concat(a).concat(".bat")};
+		Process p =  Runtime.getRuntime().exec(command);
+	} catch (IOException e){
+		System.out.println("Erro! Programa não encontrado.");
 	}
-     }
+}
+
+public static void main(String[] args) throws IOException, Exception {
+
+	DataInputStream in;
+	ServerSocket servidor = new ServerSocket(12345);
+	System.out.println("Porta 12345 aberta!");
+	String mens, aesB, ivB, mend;
+	boolean arq = false;
+	Decrypt decrypt = new Decrypt();
+	Socket cliente = servidor.accept();
+	System.out.println("Nova conexão com o cliente " +     
+	cliente.getInetAddress().getHostAddress());
+	in = new DataInputStream(cliente.getInputStream());
+	try {
+	while(!servidor.isClosed()) {
+		aesB = in.readUTF();
+		ivB = in.readUTF();
+		mens = in.readUTF();
+		mend = decrypt.decrypt(mens, aesB, ivB);
+		if(mend.equals("te amo")) {
+			System.out.println("eu sei que você me ama.");
+		} else if(mend.equals("fechar")) {
+			System.out.println("Conexão encerrada.");
+			servidor.close();
+		} else if(mend.contains("abrir")) {
+			arq = true;
+			while(arq) {
+			System.out.println("O que deseja abrir?");
+			listarquivos();
+			aesB = in.readUTF();
+			ivB = in.readUTF();
+			mens = in.readUTF();
+			mend = decrypt.decrypt(mens, aesB, ivB);
+			if(mend.equals("firefox")) {
+				exec(mend);
+			    arq = false;
+			} else {
+				arq = false;
+				}
+			} 
+		} else {
+			System.out.println("Mensagem: " + mend);
+		}
+	}
+	} catch (SocketException e) {
+		System.out.println("Conexão perdida.");
+		servidor.close();
+		}
+	}
+}
